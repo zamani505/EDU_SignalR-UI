ff3aed2d09ac541b3f99b66a77f603e9a8461d9d
m.zamani <m.zamani@DPK-100>
2023-11-09 11:59:51 +0330

    first sanple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SignalR';
+  private hubConnectionBuilder!: HubConnection;
+  message:any[]=[];
+
+  constructor(){}
+  ngOnInit(){
+    
+    // this.hubConnectionBuilder = new HubConnectionBuilder().withUrl('http://localhost:5170/message').configureLogging(LogLevel.Information).build();
+    // this.hubConnectionBuilder.start().then(() => console.log('Connection started.......!')).catch(err => console.log('Error while connect with server'));
+    // this.hubConnectionBuilder.on('SendMessageToUser', (result: any) => {
+    //  debugger;
+    //     this.message=result;
+    // });
+    this.hubConnectionBuilder = new HubConnectionBuilder().withUrl('http://localhost:5170/Chat').configureLogging(LogLevel.Information).build();
+    this.hubConnectionBuilder.start().then(() => console.log('Connection started.......!')).catch(err => console.log('Error while connect with server'));
+    this.hubConnectionBuilder.on('ReceiveMessage', (result: any) => {
+      debugger;
+         this.message=result;
+     });
+  }
 }
