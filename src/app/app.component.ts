13be54ba33254c6f1e0ef9fe3448904b14d486d0
m.zamani <m.zamani@DPK-100>
2023-11-11 09:11:34 +0330

    last change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  LogLevel,
+} from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'SignalR';
   private hubConnectionBuilder!: HubConnection;
-  message:any[]=[];
+  message: any[] = [];
 
-  constructor(){}
-  ngOnInit(){
+  constructor() {}
+  ngOnInit() {
     //دو نوع مثال ارتباط با SignalR
 
     //==================================================
     // this.hubConnectionBuilder = new HubConnectionBuilder().withUrl('http://localhost:5170/message').configureLogging(LogLevel.Information).build();//ایجاد ارتباط با هاب براساس نامی که در بک اند داده شده است مانند(message)
     // this.hubConnectionBuilder.start().then(() => console.log('Connection started.......!')).catch(err => console.log('Error while connect with server'));//شروع گوش دادن به کانال
     // this.hubConnectionBuilder.on('SendMessageToUser', (result: any) => {//SendMessageToUser نام متدی که پیام را در بک اند ارسال کرده است
-    //  debugger;
     //     this.message=result;
     // });
     //=======================================================
-    this.hubConnectionBuilder = new HubConnectionBuilder().withUrl('http://localhost:5170/Chat').configureLogging(LogLevel.Information).build();//ایجاد ارتباط با هاب براساس نامی که در بک اند داده شده است مانند(Chat)
-    this.hubConnectionBuilder.start().then(() => console.log('Connection started.......!')).catch(err => console.log('Error while connect with server'));//شروع گوش دادن به کانال
-    this.hubConnectionBuilder.on('SendAsync', (user: any,message:any) => {//SendAsync نام متدی که پیام را در بک اند ارسال کرده است
+    this.hubConnectionBuilder = new HubConnectionBuilder()
+      .withUrl('http://localhost:5170/Chat')
+      .configureLogging(LogLevel.Information)
+      .build(); //ایجاد ارتباط با هاب براساس نامی که در بک اند داده شده است مانند(Chat)
+    this.hubConnectionBuilder
+      .start()
+      .then(() => console.log('Connection started.......!'))
+      .catch((err) => console.log('Error while connect with server')); //شروع گوش دادن به کانال
+    this.hubConnectionBuilder.on('SendAsync', (user: any, message: any) => {
+      //SendAsync نام متدی که پیام را در بک اند ارسال کرده است
+      this.message.push(user + ' said ' + message);
+    });
+    //his.hubConnectionBuilder.invoke()
+    this.hubConnectionBuilder.send('Receive', (user: string) => {
       debugger;
-         this.message.push( user+" said "+message);
-     });
+    });
   }
 }
